10f5e77d2c1c20f2964c166c7d0594dcff54704d
CharlesGabin <charlesgabinnemecksongna@gmail.com>
2024-06-29 22:36:08 +0530

    modif route.ts initial

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -46,41 +46,39 @@ async function seedInvoices() {
     invoices.map(
       (invoice) => client.sql`
         INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${invoice.amount}, ${invoice.status}, ${invoice.date})
-        
+        VALUES (${invoice.customer_id}, ${invoice.amount}, ${invoice.status}, ${invoice.date})
         ON CONFLICT (id) DO NOTHING;
       `,
     ),
-    // ${invoice.customer_id}, 
   );
 
   return insertedInvoices;
 }
 
-// async function seedCustomers() {
-//   await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+async function seedCustomers() {
+  await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
-//   await client.sql`
-//     CREATE TABLE IF NOT EXISTS customers (
-//       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//       name VARCHAR(255) NOT NULL,
-//       email VARCHAR(255) NOT NULL,
-//       image_url VARCHAR(255) NOT NULL
-//     );
-//   `;
+  await client.sql`
+    CREATE TABLE IF NOT EXISTS customers (
+      id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL,
+      image_url VARCHAR(255) NOT NULL
+    );
+  `;
 
-//   const insertedCustomers = await Promise.all(
-//     customers.map(
-//       (customer) => client.sql`
-//         INSERT INTO customers (id, name, email, image_url)
-//         VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
-//         ON CONFLICT (id) DO NOTHING;
-//       `,
-//     ),
-//   );
+  const insertedCustomers = await Promise.all(
+    customers.map(
+      (customer) => client.sql`
+        INSERT INTO customers (id, name, email, image_url)
+        VALUES (${customer.id}, ${customer.name}, ${customer.email}, ${customer.image_url})
+        ON CONFLICT (id) DO NOTHING;
+      `,
+    ),
+  );
 
-//   return insertedCustomers;
-// }
+  return insertedCustomers;
+}
 
 async function seedRevenue() {
   await client.sql`
